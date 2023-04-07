880e53fa2c220a26d8c5354830370b6b0a75d82a
prinfray <paul.rinfray@gmail.com>
2023-04-07 22:48:07 +0200

    add height value

--- watintake_dribble_design/lib/widgets/daily_goal_form.dart
+++ watintake_dribble_design/lib/widgets/daily_goal_form.dart
@@ -14,6 +14,7 @@ class DailyGoalFormState extends State<DailyGoalForm> {
   double? weight;
   double? dailyGoal;
   String? unit;
+  double? height;
 
   late SharedPreferences _prefs;
 
@@ -21,77 +22,88 @@ class DailyGoalFormState extends State<DailyGoalForm> {
   void initState() {
     super.initState();
     gender = "male";
-    unit = "oz";
+    unit = "cl";
     _initPrefs();
   }
 
   Future<void> _initPrefs() async {
     _prefs = await SharedPreferences.getInstance();
   }
+void _saveFormValues() {
+  _prefs.setString('gender', gender ?? '');
+  _prefs.setDouble('weight', weight ?? 0.0);
+  _prefs.setDouble('height', height ?? 0.0);
+  _prefs.setDouble('dailyGoal', dailyGoal ?? 0.0);
+  _prefs.setString('unit', unit ?? '');
+}
 
-  void _saveFormValues() {
-    _prefs.setString('gender', gender ?? '');
-    _prefs.setDouble('weight', weight ?? 0.0);
-    _prefs.setDouble('dailyGoal', dailyGoal ?? 0.0);
-    _prefs.setString('unit', unit ?? '');
-  }
-
-  void _loadFormValues() {
-    setState(() {
-      gender = _prefs.getString('gender');
-      weight = _prefs.getDouble('weight');
-      dailyGoal = _prefs.getDouble('dailyGoal');
-      unit = _prefs.getString('unit');
-    });
-  }
+  // void _loadFormValues() {
+  //   setState(() {
+  //     gender = _prefs.getString('gender');
+  //     weight = _prefs.getDouble('weight');
+  //     dailyGoal = _prefs.getDouble('dailyGoal');
+  //     unit = _prefs.getString('unit');
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     List<Step> formSteps = [
-      Step(
-        title: const Text('Informations personnelles'),
-        content: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                ToggleButtons(
-                  isSelected: gender == 'male' || gender == null
-                      ? [true, false]
-                      : [false, true],
-                  onPressed: (index) {
-                    setState(() {
-                      gender = index == 0 ? 'male' : 'female';
-                      _saveFormValues();
-                    });
-                  },
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey[400],
-                  selectedColor: Colors.white,
-                  fillColor: Theme.of(context).primaryColor,
-                  selectedBorderColor: Theme.of(context).primaryColor,
-                  children: const <Widget>[
-                    Text('Homme'),
-                    Text('Femme'),
-                  ],
-                ),
-              ],
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Poids (en kg)'),
-              onChanged: (value) {
-                setState(() {
-                  weight = double.tryParse(value);
-                  _saveFormValues();
-                });
-              },
-              initialValue: weight?.toStringAsFixed(2),
-            ),
-          ],
-        ),
-        isActive: true,
+Step(
+  title: const Text('Informations personnelles'),
+  content: Column(
+    children: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ToggleButtons(
+            isSelected: gender == 'male' || gender == null
+                ? [true, false]
+                : [false, true],
+            onPressed: (index) {
+              setState(() {
+                gender = index == 0 ? 'male' : 'female';
+                _saveFormValues();
+              });
+            },
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[400],
+            selectedColor: Colors.white,
+            fillColor: Theme.of(context).primaryColor,
+            selectedBorderColor: Theme.of(context).primaryColor,
+            children: const <Widget>[
+              Text('Homme'),
+              Text('Femme'),
+            ],
+          ),
+        ],
       ),
+      TextFormField(
+        keyboardType: TextInputType.number,
+        decoration: const InputDecoration(labelText: 'Poids (en kg)'),
+        onChanged: (value) {
+          setState(() {
+            weight = double.tryParse(value);
+            _saveFormValues();
+          });
+        },
+        initialValue: weight?.toStringAsFixed(2),
+      ),
+      TextFormField(
+        keyboardType: TextInputType.number,
+        decoration: const InputDecoration(labelText: 'Taille (en cm)'),
+        onChanged: (value) {
+          setState(() {
+            height = double.tryParse(value);
+            _saveFormValues();
+          });
+        },
+        initialValue: height?.toStringAsFixed(2),
+      ),
+    ],
+  ),
+  isActive: true,
+),
       Step(
         title: const Text('Objectifs quotidiens'),
         content: Column(
